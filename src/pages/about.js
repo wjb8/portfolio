3a12402185be18309a31efa46d6fda316150b0fc
chinny36 <brownj95@gmail.com>
2021-07-08 14:53:40 -0400

    Setup basic pages and components

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>I'm James Brownacus</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
